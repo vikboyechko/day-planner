db374ef879d91c9331aafff0807aecae88a397b4
Vik Boyechko <vik.boyechko@gmail.com>
2023-12-02 22:47:30 -0600

    added dayjs and row hour comparisons and color classes

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+  var today = dayjs();
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,15 +13,53 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+  var scheduleEl = $('#schedule');
+  thisRowSave = scheduleEl.children().children().eq(0).children().eq(2);
+  thisRowSave.on('click', function() {
+    thisRowSave.hide(); 
+  })
+
+
+  // Compare current hour to the planner hour row and assign classes
+
+  // var thisHour = today.format('H'); // get hour in 24-hour format
+  var thisHour = '11'; // for testing
+
+  var hourElements = document.querySelectorAll('[id^="hour-"]'); // find all div id's that start with 'hour-'
+
+  for (var i = 0; i < hourElements.length; i++) {
+    var currentElement = hourElements[i]; // the current hour row ID element, e.g. hour-9
+    var currentRowHour = currentElement.id.split('-'); // split off 'hour' and '9'
+    var currentRowHourNumber = parseInt(currentRowHour[1]); // create var for '9'
+
+    if (currentRowHourNumber >= 1 && currentRowHourNumber <= 5) {
+      currentRowHourNumber += 12; 
+    }; // convert hours 1-5 to 24-hour clock
+
+    // Set the class based on if past, present, or future
+    if (currentRowHourNumber < thisHour) {
+      $(currentElement).addClass('past');
+    }
+    if (currentRowHourNumber == thisHour) {
+      $(currentElement).addClass('present');
+    }
+    if (currentRowHourNumber > thisHour) {
+      $(currentElement).addClass('future');
+    }
+
+  }
+
+  
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
+
   // TODO: Add code to display the current date in the header of the page.
+
+  $('#currentDay').text(today.format('dddd, MMMM D'))
 });
